b08b0ceb0ab12d5021b20842bb3ef356137add7c
Curtis Timson <curt@live.co.uk>
2019-07-04 17:05:41 +0100

    Release 0.1.0 (#5)
    * npm init
    
    * Initial copy of logic from flashing-page-title-notification
    
    * Copy demo from flashing-page-title-notification
    
    * Add webpack configuration
    
    * Add dist tabnotifier.min.js
    
    * update to es6 module
    
    * Add watch:true to webpack
    
    * Add constructor
    
    * Update TabNotifier to self instantiate
    
    * Update change log and package version
    
    * Release-0.1.0-dev (#3)
    
    * Update change log and version
    
    * Remove watch from webpack config
    
    * Update package version and change log

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Change Log
+
+---
+## `0.1.0`
+ - Remove watch from webpack
---
+++ dist/demo/demo.js
@@ -0,0 +1,10 @@
+var btnOn = document.getElementById('btn-on');
+var btnOff = document.getElementById('btn-off');
+
+btnOn.addEventListener('click', function(){
+    tabNotifier.notify('test', 1000);
+});
+
+btnOff.addEventListener('click', function(){
+    tabNotifier.stop();
+});
