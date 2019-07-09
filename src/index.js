39dca811a556383d924eef81c4932a13892c5e21
Curtis Timson <curt@live.co.uk>
2019-07-09 20:19:52 +0100

    Refactor state

--- src/index.js
+++ src/index.js
@@ -1,40 +1,33 @@
+import state from './state';
 import { displayNotification } from './helpers/notificationHelper';
 
 class TabNotifier {
-
-  constructor(){
-    this.state = {
-      interval: null,
-      notificationDisplayed: false,
-      originalTitle: null,
-    }
-  }
     
   notify(value, options = {
     blinkSpeed: 1000,
     replaceTitle: false,
   }) {
-    if (!this.state.originalTitle){
-      this.state.originalTitle = document.title;
+    if (!state.originalTitle){
+      state.originalTitle = document.title;
     }
-    if (!this.state.interval) {
-      this.state.interval = window.setInterval(() => {
-        if (!this.state.notificationDisplayed){
-          displayNotification(document, this.state.originalTitle, options.replaceTitle, value);
+    if (!state.interval) {
+      state.interval = window.setInterval(() => {
+        if (!state.notificationDisplayed){
+          displayNotification(document, state.originalTitle, options.replaceTitle, value);
         }
         else{
-          document.title = this.state.originalTitle;
+          document.title = state.originalTitle;
         }
-        this.state.notificationDisplayed = !this.state.notificationDisplayed;
+        state.notificationDisplayed = !state.notificationDisplayed;
       }, options.blinkSpeed);
     }
   }
 
   stop() {
-    window.clearInterval(this.state.interval);
-    this.state.interval = null;
-    document.title = this.state.originalTitle;
-    this.state.notificationDisplayed = false;
+    window.clearInterval(state.interval);
+    state.interval = null;
+    document.title = state.originalTitle;
+    state.notificationDisplayed = false;
   }
 
 }
---
+++ src/state.js
@@ -0,0 +1,7 @@
+const state = {
+  interval: null,
+  notificationDisplayed: false,
+  originalTitle: null,
+}
+
+export default state;
