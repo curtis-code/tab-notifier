fff4aef3395d1ce757843d332a9f358ed45de76e
Curtis Timson <curt@live.co.uk>
2019-09-29 18:25:08 +0100

    Update demo.js

--- dist/demo/demo.js
+++ dist/demo/demo.js
@@ -2,7 +2,9 @@ var btnOn = document.getElementById("btn-on");
 var btnOff = document.getElementById("btn-off");
 
 btnOn.addEventListener("click", function() {
-  tabNotifier.notifyMessage("test", 1000);
+  tabNotifier.notifyMessage("test", {
+    intervalSpeed: 1000
+  });
 });
 
 btnOff.addEventListener("click", function() {
