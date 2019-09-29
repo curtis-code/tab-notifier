32f33cfc88af4a14aa50b380f301dce4425304f9
Curtis Timson <curt@live.co.uk>
2019-09-29 17:08:25 +0100

    Add http-server for build test

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -10,11 +9,6 @@ module.exports = {
     libraryTarget: "window",
     path: path.resolve(__dirname, "dist")
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "src/index.html"
-    })
-  ],
   module: {
     rules: [
       {
